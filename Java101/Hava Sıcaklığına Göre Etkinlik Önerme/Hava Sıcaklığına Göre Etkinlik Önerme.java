e31ec4a3c17ba66af4623474a26645d2623f3e79
beyzahiz <130295983+beyzahiz@users.noreply.github.com>
2023-12-13 00:22:56 +0300

    Update Hava Sıcaklığına Göre Etkinlik Önerme.java

--- Java101/Hava Sıcaklığına Göre Etkinlik Önerme/Hava Sıcaklığına Göre Etkinlik Önerme.java
+++ Java101/Hava Sıcaklığına Göre Etkinlik Önerme/Hava Sıcaklığına Göre Etkinlik Önerme.java
@@ -10,13 +10,13 @@ public class EtkinlikOnerme {
 		System.out.print("Sıcaklık değerini giriniz: ");
 		sicaklik = input.nextInt();
 		
-		if(sicaklik<5) {
+		if(sicaklik<=5) {
 			System.out.println("Hava tam kayak kayma havası!");
 		}
-		else if(sicaklik>5 && sicaklik<15){
+		else if(sicaklik>5 && sicaklik<=15){
 			System.out.println("Bu havada sinema harika olur!");
 		}
-		else if(sicaklik>15 && sicaklik<25) {
+		else if(sicaklik>15 && sicaklik<=25) {
 			System.out.println("Bu güzel havayı piknikle değerlendirmeye ne dersin?");
 		}
 		else {
