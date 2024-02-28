76e191c094f2f32903eb1ba16ab63a0d873d5330
beyzahiz <130295983+beyzahiz@users.noreply.github.com>
2024-02-28 23:27:04 +0300

    Create ArtikYil.java

---
+++ Java101/Artık Yıl/ArtikYil.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ArtıkYılHesaplama {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int yil;
+		System.out.print("Yıl Giriniz: ");
+		yil = input.nextInt();
+		
+		if((yil%4 == 0)) {
+			System.out.println(yil + " bir artık yıldır!");
+		}
+		else if((yil%100 ==0) && (yil%400 !=0)) {
+			System.out.println(yil + " bir artık yıl degildir!");
+		}
+		else {
+			System.out.println(yil + " bir artık yıl degildir!");
+		}
+
+	}
+
+}
